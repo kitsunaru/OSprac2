ccd2dd80da0b80b2ecd988a13d99af596dcb280e
Silundan <git.silundan@gmail.com>
2023-09-04 14:13:02 +0930

    Update clockmmu.py
    clock

--- clockmmu.py
+++ clockmmu.py
@@ -2,13 +2,17 @@ from mmu import MMU
 
 class ClockMMU(MMU):
     def __init__(self, frames):
-        # TODO: Constructor logic for LruMMU
-        self.lru_mem_table = [None] * (frames)
+        # TODO: Constructor logic for clockMMU
+        self.clock_mem_table = [None] * (frames)
+        self.clock_index = 0
         self.dirty_arr = []
         self.read_count = 0
         self.write_count = 0
         self.fault_count = 0
         self.dbg = None
+
+    def ClockWork(self):
+        pass
 
     def set_debug(self):
         # TODO: Implement the method to set debug mode
@@ -22,22 +26,22 @@ class ClockMMU(MMU):
         # TODO: Implement the method to read memory
 
         # TODO: add a check with empty page, and adding handler for there is an empty page
-        if page_number not in self.lru_mem_table:
+        if page_number not in self.clock_mem_table:
             self.fault_count += 1
             self.read_count += 1
-            # if None not in self.lru_mem_table:
+            # if None not in self.clock_mem_table:
 
             # TODO: add element 0 dirty bit check, if it is dirty -> write to disk (w_cnt++); then pop element 0
-            if self.lru_mem_table[0] in self.dirty_arr:
+            if self.clock_mem_table[0] in self.dirty_arr:
                 self.write_count += 1
-                self.dirty_arr.remove(self.lru_mem_table[0])
+                self.dirty_arr.remove(self.clock_mem_table[0])
             if self.dbg:
                 print("--------------------------------")
                 print("######")
-                print(f"Evict victim: page number: {self.lru_mem_table[0]}")
+                print(f"Evict victim: page number: {self.clock_mem_table[0]}")
                 print("######")
-            self.lru_mem_table.pop(0)
-            self.lru_mem_table.append(page_number)
+            self.clock_mem_table.pop(0)
+            self.clock_mem_table.append(page_number)
             if self.dbg:
                 # print("--------------------------------")
                 print("Page Fault")
@@ -45,49 +49,49 @@ class ClockMMU(MMU):
             if page_number in self.dirty_arr:
                 self.dirty_arr.remove(page_number)
         else:
-            self.lru_mem_table.remove(page_number)
-            self.lru_mem_table.append(page_number)
+            self.clock_mem_table.remove(page_number)
+            self.clock_mem_table.append(page_number)
             if self.dbg:
                 # print("--------------------------------")
                 print(f"Read from memory: {page_number}")
         if self.dbg:
                 print(f"Dirty: {self.dirty_arr}")
-                print(f"stuff in memory: {self.lru_mem_table}")
+                print(f"stuff in memory: {self.clock_mem_table}")
 
     def write_memory(self, page_number):
         # TODO: Implement the method to write memory
 
-        if page_number not in self.lru_mem_table:
+        if page_number not in self.clock_mem_table:
             self.fault_count += 1
             self.read_count += 1
             if self.dbg:
                 print("--------------------------------")
                 print("Page Fault")
 
-            if self.lru_mem_table[0] in self.dirty_arr:
-                self.dirty_arr.remove(self.lru_mem_table[0])
+            if self.clock_mem_table[0] in self.dirty_arr:
+                self.dirty_arr.remove(self.clock_mem_table[0])
                 self.write_count += 1
                 if self.dbg:
-                    print(f"Disk Write: {self.lru_mem_table[0]}")
-                    print(f"stuff in memory: {self.lru_mem_table}")
+                    print(f"Disk Write: {self.clock_mem_table[0]}")
+                    print(f"stuff in memory: {self.clock_mem_table}")
 
             if self.dbg:
-                if self.lru_mem_table[0] is not None:
-                    print(f"Write: {self.lru_mem_table[0]}")
-                print(f"Evict victim: page number: {self.lru_mem_table[0]}")
-            self.lru_mem_table.pop(0)
-            self.lru_mem_table.append(page_number)
+                if self.clock_mem_table[0] is not None:
+                    print(f"Write: {self.clock_mem_table[0]}")
+                print(f"Evict victim: page number: {self.clock_mem_table[0]}")
+            self.clock_mem_table.pop(0)
+            self.clock_mem_table.append(page_number)
 
             if page_number not in self.dirty_arr:
                 self.dirty_arr.append(page_number)
             if self.dbg:
-                print(f"mem: {self.lru_mem_table}")
+                print(f"mem: {self.clock_mem_table}")
                 print(f"dirty pile: {self.dirty_arr}")
         else:
             if self.dbg:
                 print(f"In MMU Write: {page_number}")
-            self.lru_mem_table.remove(page_number)
-            self.lru_mem_table.append(page_number)
+            self.clock_mem_table.remove(page_number)
+            self.clock_mem_table.append(page_number)
             if page_number not in self.dirty_arr:
                 self.dirty_arr.append(page_number)
 
