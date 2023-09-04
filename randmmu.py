c9c76be38b9074d9a96833df107ef805297b6af8
kei <a1781646@student.adelaide.edu.au>
2023-09-04 14:04:41 +0930

    Update randmmu.py
    updated hard code of pop 0th element to pop rand_gen number

--- randmmu.py
+++ randmmu.py
@@ -3,7 +3,7 @@ from random import randint
 
 class RandMMU(MMU):
     def __init__(self, frames):
-        # TODO: Constructor logic for LruMMU
+        # TODO: Constructor logic for RandMMU
         self.rand_mem_table = [None] * (frames)
         self.dirty_arr = []
         self.read_count = 0
@@ -13,31 +13,32 @@ class RandMMU(MMU):
 
     def set_debug(self):
         # TODO: Implement the method to set debug mode
-        self.dbg = True
+        self.dbg = True # switch the debug mode to True when function is called
 
     def reset_debug(self):
         # TODO: Implement the method to reset debug mode
-        self.dbg = False
+        self.dbg = False # reset debug mode to False when function is called
 
     def read_memory(self, page_number):
         # TODO: Implement the method to read memory
 
         # TODO: add a check with empty page, and adding handler for there is an empty page
+        rand_gen = random.randint(0, (frames-1))
         if page_number not in self.rand_mem_table:
             self.fault_count += 1
             self.read_count += 1
             # if None not in self.rand_mem_table:
 
             # TODO: add element 0 dirty bit check, if it is dirty -> write to disk (w_cnt++); then pop element 0
-            if self.rand_mem_table[0] in self.dirty_arr:
+            if self.rand_mem_table[rand_gen] in self.dirty_arr:
                 self.write_count += 1
-                self.dirty_arr.remove(self.rand_mem_table[0])
+                self.dirty_arr.remove(self.rand_mem_table[rand_gen])
             if self.dbg:
                 print("--------------------------------")
                 print("######")
-                print(f"Evict victim: page number: {self.rand_mem_table[0]}")
+                print(f"Evict victim: page number: {self.rand_mem_table[rand_gen]}")
                 print("######")
-            self.rand_mem_table.pop(0)
+            self.rand_mem_table.pop(rand_gen)
             self.rand_mem_table.append(page_number)
             if self.dbg:
                 # print("--------------------------------")
@@ -57,7 +58,7 @@ class RandMMU(MMU):
 
     def write_memory(self, page_number):
         # TODO: Implement the method to write memory
-
+        rand_gen = random.randint(0, (frames-1))
         if page_number not in self.rand_mem_table:
             self.fault_count += 1
             self.read_count += 1
@@ -65,18 +66,18 @@ class RandMMU(MMU):
                 print("--------------------------------")
                 print("Page Fault")
 
-            if self.rand_mem_table[0] in self.dirty_arr:
-                self.dirty_arr.remove(self.rand_mem_table[0])
+            if self.rand_mem_table[rand_gen] in self.dirty_arr:
+                self.dirty_arr.remove(self.rand_mem_table[rand_gen])
                 self.write_count += 1
                 if self.dbg:
-                    print(f"Disk Write: {self.rand_mem_table[0]}")
+                    print(f"Disk Write: {self.rand_mem_table[rand_gen]}")
                     print(f"stuff in memory: {self.rand_mem_table}")
 
             if self.dbg:
                 if self.rand_mem_table[0] is not None:
-                    print(f"Write: {self.rand_mem_table[0]}")
-                print(f"Evict victim: page number: {self.rand_mem_table[0]}")
-            self.rand_mem_table.pop(0)
+                    print(f"Write: {self.rand_mem_table[rand_gen]}")
+                print(f"Evict victim: page number: {self.rand_mem_table[rand_gen]}")
+            self.rand_mem_table.pop(rand_gen)
             self.rand_mem_table.append(page_number)
 
             if page_number not in self.dirty_arr:
